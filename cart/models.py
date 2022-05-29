08900f84e1c33d32e746cb4c1a3b007aab422689
Oyedele Olufemi <nwokoronicholas@yahoo.com>
2022-05-29 11:16:04 +0100

    Added-Cart-View

--- cart/models.py
+++ cart/models.py
@@ -1,4 +1,3 @@
-from tabnanny import verbose
 from django.contrib.auth import get_user_model
 from django.db import models
 from django.db.models.signals import pre_save
--- cart/utils.py
+++ cart/utils.py
@@ -7,7 +7,7 @@ def get_or_set_order_session(request):
     if order_id is None:
         order = Order()
         order.save()
-        request.session['order_id'] = order_id
+        request.session['order_id'] = order.id
         
     else:
         try:
@@ -15,11 +15,11 @@ def get_or_set_order_session(request):
         except Order.DoesNotExist:
             order = Order()
             order.save()
-            request.session['order_id'] = order_id
+            request.session['order_id'] = order.id
             
     if request.user.is_authenticated and order.user is None:
-        order.user == request.user
+        order.user = request.user
         order.save()
-        return order
+    return order
         
     
