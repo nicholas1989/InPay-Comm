a0baddc4195eb69d22e301c9e585bcff39a797ef
Oyedele Olufemi <nwokoronicholas@yahoo.com>
2022-05-29 11:40:50 +0100

    Added-Prices

--- cart/models.py
+++ cart/models.py
@@ -50,6 +50,7 @@ class Product(models.Model):
     slug = models.SlugField(unique=True)
     image = models.ImageField(upload_to='product_images')
     description = models.TextField()
+    price = models.IntegerField(default=0)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now_add=True)
     active = models.BooleanField(default=False)
@@ -63,7 +64,8 @@ class Product(models.Model):
     def get_absolute_url(self):
         return reverse("cart:product-detail", kwargs={'slug': self.slug})
     
-    
+    def get_price(self):
+        return "{:.2f}".format(self.price / 100)
     
 class OrderItem(models.Model):
     order = models.ForeignKey("Order", related_name='items', on_delete=models.CASCADE)
@@ -74,6 +76,13 @@ class OrderItem(models.Model):
     
     def __str__(self):
         return f"{self.quantity} x {self.product.title}"
+    
+    def get_raw_total_item_price(self):
+        return self.quantity * self.product.price
+    
+    def get_total_item_price(self):
+        price = self.get_raw_total_item_price() # raw format of 1000
+        return "{:.2f}".format(price / 100)
     
     
 class Order(models.Model):
